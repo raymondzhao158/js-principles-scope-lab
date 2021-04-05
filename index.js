88c6568caa6f4fe8fcd6511daa6ce8d68b8d59d0
raymond zhao <raymondzhao158@gmail.com>
2021-04-05 15:34:10 -0700

    commit tree fd4e0f2e51e66f2a6251f26bafb15dc7fb6c0449

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+};
+
+function setBestCustomer(name=`not bob`){
+    bestCustomer = name
+};
+
+function overwriteBestCustomer(name=`bob`) {
+    bestCustomer = name
+};
+
+const leastFavoriteCustomer = `karen`
+
+function changeLeastFavoriteCustomer(name=`steve`) {
+    leastFavoriteCustomer = name
+};
